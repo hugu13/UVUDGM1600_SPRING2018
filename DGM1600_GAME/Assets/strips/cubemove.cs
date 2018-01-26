27cf5feab381c6a20ee8865e0cda623bf4442295
Unknown <HMartinezlk@gmail.com>
2018-01-25 19:26:05 -0700

    controller

--- DGM1600_GAME/Assets/strips/cubemove.cs
+++ DGM1600_GAME/Assets/strips/cubemove.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class cubemove : MonoBehaviour {
 
-	public float transX;
-	public float transY;
-	public float transZ;
+	// public float transX;
+	// public float transY;
+	// public float transZ;
 
-	public float rotX;
-	public float rotY;
-	public float rotZ;
+	// public float rotX;
+	// public float rotY;
+	// public float rotZ;
 
+		public Vector3 move;
+		public Vector3 rotate;
+		public float moveSpeed;
 	
 	// Use this for initialization
 	void Start () {
@@ -20,7 +23,24 @@ public class cubemove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(transX,transY,transZ);
-		transform. Rotate(rotX, rotY, rotZ);
+		// transform.Translate(transX,transY,transZ);
+		// transform. Rotate(rotX, rotY, rotZ);
+		if(Input.GetKey(KeyCode.W))
+			transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+		// transform.Translate(move * Time.deltaTime);
+		// transform.Rotate(rotate * Time.deltaTime);
+		if(Input.GetKey(KeyCode.S))
+			transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+			
+		if(Input.GetKey(KeyCode.D))
+			transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+		if(Input.GetKey(KeyCode.A))
+			transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+
 	}
-}
+
+		
+		}
+			
+
---
+++ DGM1600_GAME/Assets/strips/playerMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour {
+	public float moveSpeed;
+	public float turnSpeed;
+	public float jumpHeight;
+
+
+	// Use this for initialization
+	void Start () {
+		var j = Input.GetAxis("Jump")* Time.deltaTime * jumpHeight;
+		var y = Input.GetAxis("Horizontal")* Time.deltaTime * turnSpeed;
+		var z = Input.GetAxis("Vertical")* Time.deltaTime * moveSpeed;
+
+		transform.Rotate(0,y,0);
+		transform.Translate(0,0,z);
+		transform.Translate(0,j,0);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
