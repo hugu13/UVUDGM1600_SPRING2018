8d960da43664e0287fb4685e04477d2b4c5db74e
Unknown <HMartinezlk@gmail.com>
2018-03-01 19:53:14 -0700

    loops
    created loops

---
+++ DGM1600_GAME/Assets/strips/Loops.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+	public int beer = 99; 
+
+	public int bacon =9;
+	// Use this for initialization
+	void Start () {
+		// for(int i=0;i > beer; i++){
+			// print(i +"bottles of beer on the wall")
+		// }
+
+		// while(bacon>0){
+			// print('mmmm bacon!!!'+bacon);
+
+			// bacon --;
+		// }
+		// if bacon (<=0){
+			// print("ohh out of bacon!!");
+		// }
+
+		// bool isFired = false;
+
+		// do{
+			// print("you're fired!!! - Donald Trump");
+		// }
+		// while(isFired == true);
+		
+		string[] churro = new string[3];
+
+		churro[0] = "first churro, yum!";
+		churro[1] = "second churro, ehh";
+		churro[2] = "Third churrro, uueghh!";
+
+		foreach(string item in churro){
+			print(item);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- DGM1600_GAME/Assets/strips/Wonder.cs
+++ DGM1600_GAME/Assets/strips/Wonder.cs
@@ -18,14 +18,14 @@ public class Wonder : MonoBehaviour {
 
 	void Wondering(){
 		print("wolf is Wondering");
-		transform.Translate(vector3.forward*moveSpeed*Time.deltaTime);
+		transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
 		int randomNum = Random.Range(0,360);
 		Vector3 fwd = transform.TransformDirection(Vector3.forward);
-		Raycasthit hit;
+		RaycastHit hit;
 
-		Debug.Drawray(transform.position,fwd*3,Color.red);
+		Debug.DrawRay(transform.position,fwd*3,Color.red);
 
-		if(physics.Raycast(transform.position,fwd,out hit,3)){
+		if(Physics.Raycast(transform.position,fwd,out hit,3)){
 
 			if(hit.collider.tag == "wall"){
 				transform.Rotate(0,randomNum,0);
@@ -36,15 +36,23 @@ public class Wonder : MonoBehaviour {
 	void Following(){
 		print("wolf is following");
 		transform.LookAt(target);
-		transform.Translate(vector3.forward*moveSpeed*Time.deltaTime);
+		transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
 	}
 
-	void OnTriggerStay(collider other)
+	void OnTriggerStay(Collider other)
 {
-	if(other.gameObject.name == "Player")
-			print("Player stinks!");
-			//Following();
+	if(other.gameObject.name == "Player"){
+		print("Player stinks!");
+	}
+	else{
+		Wondering();
+	}
 
+
+	
+			
+			//Following();
+		
 }
 		
 
