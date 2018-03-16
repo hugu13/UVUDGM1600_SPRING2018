99130def77c7c8ee80e6e8b15279ebb413cfa1ac
Unknown <HMartinezlk@gmail.com>
2018-03-15 19:36:19 -0600

    UI
    Mde a Main Menu, button clicks and everything.

---
+++ DGM1600_GAME/Assets/strips/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public int LevelToLoad;
+
+
+	// Use this for initialization
+	public void LoadLevel () {
+		SceneManager.LoadScene(LevelToLoad);
+		
+	}
+	
+	// Update is called once per frame
+	public void LevelExit () {
+		Application.Quit();
+		
+	}
+}
