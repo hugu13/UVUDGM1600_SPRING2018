e7091b1a0847d86e7e47bf7570cd562dcf5567de
Unknown <HMartinezlk@gmail.com>
2018-03-29 19:59:46 -0600

    scoremanager
    health

---
+++ DGM1600_GAME/Assets/strips/Navwander.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navwander : MonoBehaviour {
+
+	public float wanderRadius;
+	public float wanderTimer;
+
+
+	private UnityEngine.AI.NavMeshAgent Agent;
+	private Transform target;
+	private float timer;
+
+	public Transform enemy;
+	public Transform chickenpen;
+
+	public int points;
+	public bool isWandering = true;
+	
+
+	// Use this for initialization
+	void OnEnable () {
+		Agent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+		timer = wanderTimer;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+
+		if(timer>= wanderTimer) {
+			Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+			Agent.SetDestination(newPos);
+			timer = 0;
+		}
+		
+	}
+	public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask) {
+		Vector3 randDirection = Random.insideUnitSphere * dist;
+		randDirection += origin;
+		UnityEngine.AI.NavMeshHit navHit;
+		UnityEngine.AI.NavMesh.SamplePosition (randDirection, out navHit, dist, layermask);
+		return navHit.position;
+	}
+	//Chicken Interactions
+	//void OnTriggerStay(Collider other)
+	//{
+		//if(other.)
+	//}
+}
+
---
+++ DGM1600_GAME/Assets/strips/scoreManager.cs
@@ -0,0 +1,50 @@
+﻿using  System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class scoreManager : MonoBehaviour {
+
+	public static int score;
+	public int winScore;
+	public Text text;
+
+	public Text winText;
+	// Use this for initialization
+	void Awake () {
+		Time.timeScale = 1;
+
+		
+		
+	}
+	
+	// Update is called once per frame
+	void start(){
+		winText.GetComponent<Text>().enable = false;
+		text = GetComponent<Text>();
+		score= 0;
+	}
+
+	void Update (){
+		if(score< 0)
+		score = 0;
+
+		text.text= " "+ score;
+
+		if(winScore == score){
+			print("win score reached =" + score);
+			winText.GetComponent<Text>().enable = true;
+		}
+		if(input.GetKeyDown(KeyCode.Escape)){
+			scoreManager.LoadScene(0);
+		}
+	}
+		
+	public static void AddPoints(int pointsToADD){
+		score += pointsToADD;
+	}
+	public void Reset(){
+		score=0;
+	}
+}
