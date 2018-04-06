f78d0a1c74b3306eba7f8cae373cee569ad4e907
Unknown <HMartinezlk@gmail.com>
2018-04-05 20:27:56 -0600

    navmesh
    navmesh wolf and ai

--- DGM1600_GAME/Assets/strips/scoreManager.cs
+++ DGM1600_GAME/Assets/strips/scoreManager.cs
@@ -1,50 +1,16 @@
-﻿using  System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-
 
 public class scoreManager : MonoBehaviour {
 
-	public static int score;
-	public int winScore;
-	public Text text;
-
-	public Text winText;
 	// Use this for initialization
-	void Awake () {
-		Time.timeScale = 1;
-
-		
+	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void start(){
-		winText.GetComponent<Text>().enable = false;
-		text = GetComponent<Text>();
-		score= 0;
-	}
-
-	void Update (){
-		if(score< 0)
-		score = 0;
-
-		text.text= " "+ score;
-
-		if(winScore == score){
-			print("win score reached =" + score);
-			winText.GetComponent<Text>().enable = true;
-		}
-		if(input.GetKeyDown(KeyCode.Escape)){
-			scoreManager.LoadScene(0);
-		}
-	}
+	void Update () {
 		
-	public static void AddPoints(int pointsToADD){
-		score += pointsToADD;
-	}
-	public void Reset(){
-		score=0;
 	}
 }
